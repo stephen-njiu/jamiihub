556510cc4710fdda7b93de3b643c1306d9a8b866
VynceUpp <92018011+VynceUpp@users.noreply.github.com>
2024-10-20 06:54:30 +0300

    root route for the api

--- backend/server.js
+++ backend/server.js
@@ -168,6 +168,11 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: "Internal Server Error" });
 });
 
+//root route
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port${port}`);
